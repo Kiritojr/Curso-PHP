10ef68c3a773dfa376e731e9e0d268b368f4e705
Roberto Jr <junionascimento2001@gmail.com>
2020-07-04 18:17:17 -0300

    function load

--- public/index.php
+++ public/index.php
@@ -4,9 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheep" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"/>
+
   <title>PHP para quem sabe - curso</title>
 </head>
-<body>
-  
-</body>
+  <body>
+
+    <?php 
+      require load();
+    ?>
+    
+  </body>
 </html>
